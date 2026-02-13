c19edad20c3d2012577ea43d3b1ab05816ce2365
aymanebenomar <aymanebenomar2005@gmail.com>
2026-02-13 19:37:32 +0100

    linking the db with migration and also creating a user with tinker  + creating a contoller for auth

--- logindash/routes/web.php
+++ logindash/routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function ($email = 'Enter Your Email') {
+    return view('login', [
+        'email' => $email
+    ]);
+});
+
+
+Route::get('/test', function($test = 'Test')
+{
+    return view('test', [
+        'test' => $test
+    ]);
+});
