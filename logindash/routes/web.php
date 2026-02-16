03d5d8c845766945ff3aa1035778308798663245
aymanebenomar <aymanebenomar2005@gmail.com>
2026-02-16 17:42:20 +0000

    updating

--- logindash/routes/web.php
+++ logindash/routes/web.php
@@ -6,30 +6,21 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-
-// Auth pages
-
-Route::get('/login', [AuthController::class, 'showLogin']);
+// Auth
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/register', [AuthController::class, 'showRegister']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Posts (feed public, actions protected)
-
+// Posts
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/{id}', [PostController::class, 'store'])->name('posts.show');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::middleware('auth')->group(function() {
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
-
-
-
-// dash 
-// Route::middleware('auth')->get('/dashboard', function () {
-//     return view('dashboard');
-// })->name('dashboard');
