ce6cf9b6e8648988f4dcc89b3fbbb7f2aa4768f4
aymanebenomar <aymanebenomar2005@gmail.com>
2026-03-04 11:36:18 +0000

    test

--- revision/app/Http/Controllers/Controller.php
+++ revision/app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use App\Models\Stagiaire;
+
+class StagiaireController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        $stagiaires = Stagiaire::all();
+        return view('stagiaires.index', compact('stagiaires'));
+    }
+
+
+    public function store(Request $request)
+    {
+        Stagiaire::create([
+            'name' => $request->name,
+            'specialite' => $request->specialite,
+            'note' => $request->note
+        ]);
+
+        return redirect('/stagiaires');
+    }
+
+    
+    public function destroy($id)
+    {
+        Stagiaire::find($id)->delete();
+        return redirect('/stagiaires');
+    }
 }
