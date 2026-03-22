6f0a043eb8ea34d360a29ecef096ffdee4898132
aymanebenomar <aymanebenomar2005@gmail.com>
2026-03-22 14:59:26 +0100

    updating web

--- exam/routes/web.php
+++ exam/routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StagiaireController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('stagiaires', StagiaireController::class);
