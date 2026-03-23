97f85275b26f14f70481dbe3d02f78da07960abb
aymanebenomar <aymanebenomar2005@gmail.com>
2026-03-23 09:10:30 +0100

    doing the controller and model again

---
+++ rev/app/Http/Controllers/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'email' => 'required',
+            'filiere' => 'required',
+            'age' => 'required',
+        ]);
+
+        Student::create($request->all());
+        return redirect()->route('students.index')->with('success', 'student added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(student $student)
+    {
+        return view('students.info', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(student $student)
+    {
+        return view('students.edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, student $student)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'email' => 'required',
+            'filiere' => 'required',
+            'age' => 'required',
+        ]);
+
+        $student->update($request->all());
+        return redirect()->route('students.index')->with('success', 'student updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(student $student)
+    {
+        $student->delete();
+        return redirect()->route('students.index')->with('success', 'student deleted successfully');
+    }
+}
