5fe39e366cd8f048aaddc6d6f4153110f898a9f2
aymanebenomar <aymanebenomar2005@gmail.com>
2026-04-15 15:03:52 +0100

    migration files done

---
+++ exam/database/migrations/2026_04_15_134344_create_commande_produit_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commande_produit', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignid('commande_id')->constrained()->onDelete('cascade');
+            $table->foreignid('produit-id')->constrained()->onDelete('cascade');
+
+            $table->integer('quantite');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commande_produit');
+    }
+};
